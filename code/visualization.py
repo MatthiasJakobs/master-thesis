ec0e457cf1bd227887aa32dfcad91c2430975ef9
Matthias Jakobs <m.jakobs265@gmail.com>
2019-07-22 13:22:37 +0200

    plot whole skeleton instead of one image per joint

--- code/visualization.py
+++ code/visualization.py
@@ -1,5 +1,5 @@
 import matplotlib
-matplotlib.use('Agg')
+#matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
@@ -12,7 +12,7 @@ import os
 
 import torch
 
-from deephar.utils import transform_2d_point
+from deephar.utils import transform_2d_point, transform_pose
 
 def show_pose(clip, idx):
     image = clip["images"][idx]
@@ -77,36 +77,63 @@ def show_pose_mpii(annotation):
     plt.show()
 
 def show_predictions_ontop(ground_truth, image, poses, path, matrix, original_size):
-    fig, ax = plt.subplots(nrows=4, ncols=4, figsize=(10, 10))
 
-    for i, axi in enumerate(ax.flat):
-        axi.set_xticks([])
-        axi.set_yticks([])
+    plt.xticks([])
+    plt.yticks([])
+    
+    ground_pose = ground_truth[:, 0:2]
+    predicted_pose = poses[:, 0:2]
 
-        if torch.cuda.is_available():
-            pose_coordinates = poses[i].cpu().detach().numpy()
-            gt_coordintates = ground_truth[i].cpu().detach().numpy()
-        else:
-            pose_coordinates = poses[i].detach().numpy()
-            gt_coordintates = ground_truth[i].detach().numpy()
+    plt.imshow(image)
 
-        axi.imshow(image)
+    orig_gt_coordinates = transform_pose(matrix, ground_pose, inverse=True)
+    orig_pred_coordinates = transform_pose(matrix, predicted_pose, inverse=True)
 
-        vis = pose_coordinates[2]
-        if ground_truth[i, 2] == 0.0:
-            axi.set_title(mpii_joint_order[i] + " not visible")
+    gt_color = "r"
+    pred_color = "b"
+
+    mapping = [
+        [0, 1],
+        [1, 2],
+        [2, 6],
+        [3, 6],
+        [4, 3],
+        [5, 4],
+        [6, 7],
+        [7, 8],
+        [8, 9],
+        [10, 11],
+        [11, 12],
+        [12, 7],
+        [13, 7],
+        [14, 13],
+        [15, 14]
+    ]
+
+    for i, (src, dst) in enumerate(mapping):
+        if not ground_truth[src, 2]:
             continue
+
+        if ground_truth[dst, 2]:
+            #print("{} => {}".format(mpii_joint_order[src], mpii_joint_order[dst]))
+            plt.plot([orig_gt_coordinates[src][0], orig_gt_coordinates[dst][0]], [orig_gt_coordinates[src][1], orig_gt_coordinates[dst][1]], lw=1, c=gt_color)
+            plt.plot([orig_pred_coordinates[src][0], orig_pred_coordinates[dst][0]], [orig_pred_coordinates[src][1], orig_pred_coordinates[dst][1]], lw=1, c=pred_color)
+
+            plt.scatter([orig_gt_coordinates[src][0], orig_gt_coordinates[dst][0]], [orig_gt_coordinates[src][1], orig_gt_coordinates[dst][1]], c=gt_color)
+            plt.scatter([orig_pred_coordinates[src][0], orig_pred_coordinates[dst][0]], [orig_pred_coordinates[src][1], orig_pred_coordinates[dst][1]], c=pred_color)
         else:
-            axi.set_title(mpii_joint_order[i] + " vis: {0:.2f}".format(vis))
+            #print("{}".format(mpii_joint_order[src]))
+            plt.scatter(orig_gt_coordinates[src][0], orig_gt_coordinates[src][1], c=gt_color)
+            plt.scatter(orig_pred_coordinates[src][0], orig_pred_coordinates[src][1], c=pred_color)
 
-        gt_coordintates = transform_2d_point(matrix, gt_coordintates[0:2], inverse=True)
-        pose_coordinates = transform_2d_point(matrix, pose_coordinates[0:2], inverse=True)
+        
 
-        axi.scatter(gt_coordintates[0], gt_coordintates[1], c="b")
-        axi.scatter(pose_coordinates[0], pose_coordinates[1], c="#FF00FF")
 
     if os.path.isfile(path):
         os.remove(path)
-    plt.savefig(path, pad_inches=0.01)
+    plt.savefig(path)
     plt.close()
+    #plt.pause(0.001)
+    #plt.show()
+
 
