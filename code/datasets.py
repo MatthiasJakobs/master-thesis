c60562355dbe83a7c8d9f1a6bc07f537ace48176
Matthias Jakobs <m.jakobs265@gmail.com>
2019-08-22 17:36:51 +0200

    added the first version of new sampler I might use

--- code/datasets.py
+++ code/datasets.py
@@ -1,6 +1,8 @@
 import torch
 import torch.utils.data as data
 
+import random
+
 import os
 import re
 import glob
@@ -800,6 +802,69 @@ class ImbalancedDatasetSampler(data.sampler.Sampler):
         return (self.indices[i] for i in torch.multinomial(
             self.weights, self.num_samples, replacement=True))
 
+    def __len__(self):
+        return self.num_samples
+
+class EqualFrameLengthSampler(data.sampler.Sampler):
+    """Samples elements randomly from a given list of indices for imbalanced dataset
+    Arguments:
+        indices (list, optional): a list of indices
+        num_samples (int, optional): number of samples to draw
+    """
+
+    def __init__(self, dataset, indices=None, use_random=True, num_samples=None):
+
+        # if indices is not provided,
+        # all elements in the dataset will be considered
+        self.indices = list(range(len(dataset))) \
+            if indices is None else indices
+
+        if use_random:
+            random.shuffle(self.indices)
+
+        # if num_samples is not provided,
+        # draw `len(indices)` samples in each iteration
+        self.num_samples = len(self.indices) \
+            if num_samples is None else num_samples
+
+        # distribution of classes in the dataset
+        self.frame_lengths = {}
+        for idx in self.indices:
+            label = self._get_label(dataset, idx)
+            if label in self.frame_lengths:
+                self.frame_lengths[label].append(idx)
+            else:
+                self.frame_lengths[label] = [idx]
+
+        for item in self.frame_lengths.keys():
+            if len(self.frame_lengths[item]) % 2 == 1:
+                self.frame_lengths[item] = self.frame_lengths[item][1:]
+
+
+    def _get_label(self, dataset, idx):
+        return dataset[idx]["sequence_length"].item()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        keys = list(self.frame_lengths.keys())
+        random.shuffle(keys)
+
+        for key in keys:
+            if len(self.frame_lengths[key]) == 0:
+                continue
+
+            sample_indices = random.sample(self.frame_lengths[key], 2)
+            samples = []
+            for sample_idx in sample_indices:
+                samples.append(sample_idx)
+                self.frame_lengths[key].remove(sample_idx)
+            return samples
+
+        raise StopIteration
+
+    
     def __len__(self):
         return self.num_samples
 
