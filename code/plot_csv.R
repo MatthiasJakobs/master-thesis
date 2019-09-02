f24acb207efffbcb587a2d1fd9ee23835db76ae9
Matthias Jakobs <m.jakobs265@gmail.com>
2019-09-02 10:07:57 +0200

    added R plotting function

---
+++ code/plot_csv.R
@@ -0,0 +1,9 @@
+par(mfrow=c(1,2))
+
+path = "/tmp/fernando_friday/"
+
+loss.data = read.csv(file=paste(path,"loss.csv", sep=""))
+val.data = read.csv(file=paste(path,"validation.csv", sep=""))
+
+plot(x = loss.data$iteration, y = loss.data$loss, type="l", main = "training loss", xlab ="iterations", ylab = "loss")
+plot(val.data$iteration, val.data$pckh_0.5, col="red", type="l", ylab="PCKh @ 0.5", xlab="iterations", main="validation accuracy")
