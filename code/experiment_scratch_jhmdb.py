c59b9ae33fae51cecde49a5311c816caa08b2f6f
Matthias Jakobs <matthias.jakobs@tu-dortmund.de>
2019-09-11 08:29:09 +0200

    added scratch training pose experiment for JHMDB

---
+++ code/experiment_scratch_jhmdb.py
@@ -0,0 +1,18 @@
+from ExperimentBase import Pose_JHMDB
+
+conf = {}
+
+conf["batch_size"] = 1
+conf["learning_rate"] = 1e-5
+conf["nr_epochs"] = 100
+conf["validation_amount"] = 0.1
+conf["limit_data_percent"] = 1
+conf["numpy_seed"] = 30004
+conf["name"] = "scratch_jhmdb"
+conf["num_blocks"] = 4
+conf["nr_context"] = 0
+conf["project_dir"] = ""
+conf["evaluate_rate"] = 1000
+
+ft = Pose_JHMDB(conf, use_pretrained=False)
+ft.run_experiment()
