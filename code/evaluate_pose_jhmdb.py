9423cd058325cefb86cd123426a2e3417eed5eae
Matthias Jakobs <m.jakobs265@gmail.com>
2019-10-29 13:55:10 +0100

    script for computing JHMDB test scores

--- code/evaluate_pose_jhmdb.py
+++ code/evaluate_pose_jhmdb.py
@@ -3,7 +3,7 @@ from ExperimentBase import *
 conf = {}
 
 # example values, need to be present but are not important
-conf["batch_size"] = 2
+conf["batch_size"] = 1
 conf["val_batch_size"] = conf["batch_size"]
 conf["learning_rate"] = 2e-5
 conf["nr_epochs"] = 150
@@ -13,12 +13,46 @@ conf["num_blocks"] = 4
 conf["use_saved_tensors"] = True
 conf["nr_context"] = 0
 conf["project_dir"] = ""
-conf["total_iterations"] = 3
-conf["evaluate_rate"] = 3
+conf["total_iterations"] = 1
+conf["evaluate_rate"] = 1
 conf["name"] = "eval_pose_jhmdb"
 
 
 conf["limit_data_percent"] = 1 # limit dataset to x percent (for testing)
 
 model = Pose_JHMDB(conf, validate=True)
-print(model.test(pretrained_model="/data/mjakobs/data/pretrained_jhmdb"))
+print("without context, nr_blocks = 2")
+print("refined")
+print(model.test(pretrained_model="/data/mjakobs/code/master-thesis/code/experiments/scratch_jhmdb/without_context_2/weights/weights_00130000", refine_bounding_box=True))
+print("not refined")
+print(model.test(pretrained_model="/data/mjakobs/code/master-thesis/code/experiments/scratch_jhmdb/without_context_2/weights/weights_00130000", refine_bounding_box=False))
+
+print("without context, nr_blocks = 4")
+print("refined")
+print(model.test(pretrained_model="/data/mjakobs/code/master-thesis/code/experiments/scratch_jhmdb/without_context_4/weights/weights_00160000", refine_bounding_box=True))
+print("not refined")
+print(model.test(pretrained_model="/data/mjakobs/code/master-thesis/code/experiments/scratch_jhmdb/without_context_4/weights/weights_00160000", refine_bounding_box=False))
+
+# print("without context, nr_blocks = 8")
+# print("refined")
+# print(model.test(pretrained_model="/data/mjakobs/code/master-thesis/code/experiments/scratch_jhmdb/without_context_8/weights/weights_00", refine_bounding_box=True))
+# print("not refined")
+# print(model.test(pretrained_model="/data/mjakobs/code/master-thesis/code/experiments/scratch_jhmdb/without_context_8/weights/weights_00", refine_bounding_box=False))
+
+print("with context, nr_blocks = 2")
+print("refined")
+print(model.test(pretrained_model="/data/mjakobs/code/master-thesis/code/experiments/scratch_jhmdb/with_context_2/weights/weights_00140000", refine_bounding_box=True))
+print("not refined")
+print(model.test(pretrained_model="/data/mjakobs/code/master-thesis/code/experiments/scratch_jhmdb/with_context_2/weights/weights_00140000", refine_bounding_box=False))
+
+print("with context, nr_blocks = 4")
+print("refined")
+print(model.test(pretrained_model="/data/mjakobs/code/master-thesis/code/experiments/scratch_jhmdb/with_context_4/weights/weights_00160000", refine_bounding_box=True))
+print("not refined")
+print(model.test(pretrained_model="/data/mjakobs/code/master-thesis/code/experiments/scratch_jhmdb/with_context_4/weights/weights_00160000", refine_bounding_box=False))
+
+# print("with context, nr_blocks = 8")
+# print("refined")
+# print(model.test(pretrained_model="/data/mjakobs/code/master-thesis/code/experiments/scratch_jhmdb/with_context_8/weights/weights_00", refine_bounding_box=True))
+# print("not refined")
+# print(model.test(pretrained_model="/data/mjakobs/code/master-thesis/code/experiments/scratch_jhmdb/with_context_8/weights/weights_00", refine_bounding_box=False))
