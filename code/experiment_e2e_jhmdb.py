afb4c84b0b23e2d2774044677060120234789cdd
Matthias Jakobs <m.jakobs265@gmail.com>
2019-11-10 17:15:30 +0100

    e2e time distributed

--- code/experiment_e2e_jhmdb.py
+++ code/experiment_e2e_jhmdb.py
@@ -2,9 +2,9 @@ from ExperimentBase import HAR_E2E
 
 conf = {}
 
-conf["batch_size"] = 12
+conf["batch_size"] = 1
 conf["val_batch_size"] = 1
-conf["learning_rate"] = 1e-6
+conf["learning_rate"] = 1e-5
 conf["validation_amount"] = 0.1 # 10 percent
 conf["limit_data_percent"] = 1 # limit dataset to x percent (for testing)
 conf["numpy_seed"] = 30004
@@ -15,6 +15,7 @@ conf["total_iterations"] = 50000
 conf["evaluate_rate"] = 1
 conf["name"] = "har_e2e_jhmdb"
 conf["use_gt_bb"] = True
+conf["use_timedistributed"] = True
 
 har = HAR_E2E(conf)
 har.run_experiment()
