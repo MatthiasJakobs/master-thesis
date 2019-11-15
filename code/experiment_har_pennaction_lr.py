c034c7f698d6eaa7608695eaaede31659094c788
Matthias Jakobs <m.jakobs265@gmail.com>
2019-11-15 16:38:05 +0100

    added experiment for determining learning rate for penn aciton HAR

---
+++ code/experiment_har_pennaction_lr.py
@@ -0,0 +1,44 @@
+from ExperimentBase import HAR_PennAction
+
+conf = {}
+
+conf["validation_amount"] = 0.1 # 10 percent
+conf["numpy_seed"] = 30004
+conf["num_blocks"] = 4
+conf["nr_context"] = 2
+conf["project_dir"] = "har_pennaction_determine_lr"
+
+conf["batch_size"] = 12
+conf["val_batch_size"] = 1
+conf["total_iterations"] = 25000
+conf["evaluate_rate"] =  500
+conf["limit_data_percent"] = 1 # limit dataset to x percent (for testing)
+conf["use_gt_bb"] = True
+conf["use_gt_pose"] = False
+conf["use_timedistributed"] = True
+conf["fine_tune"] = False 
+
+conf["learning_rate"] = 1e-2
+conf["name"] = "1e-2"
+har = HAR_PennAction(conf)
+har.run_experiment()
+
+conf["learning_rate"] = 1e-3
+conf["name"] = "1e-3"
+har = HAR_PennAction(conf)
+har.run_experiment()
+
+conf["learning_rate"] = 1e-4
+conf["name"] = "1e-4"
+har = HAR_PennAction(conf)
+har.run_experiment()
+
+conf["learning_rate"] = 1e-5
+conf["name"] = "1e-5"
+har = HAR_PennAction(conf)
+har.run_experiment()
+
+conf["learning_rate"] = 1e-6
+conf["name"] = "1e-6"
+har = HAR_PennAction(conf)
+har.run_experiment()
