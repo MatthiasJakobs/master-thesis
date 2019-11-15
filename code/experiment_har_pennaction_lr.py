6aedc7df98f15e5dae60a69d8157ff1c3278aae6
Matthias Jakobs <matthias.jakobs@tu-dortmund.de>
2019-11-15 16:43:54 +0100

    changed learning rate and removed 1e-2 because NaN error

--- code/experiment_har_pennaction_lr.py
+++ code/experiment_har_pennaction_lr.py
@@ -8,7 +8,7 @@ conf["num_blocks"] = 4
 conf["nr_context"] = 2
 conf["project_dir"] = "har_pennaction_determine_lr"
 
-conf["batch_size"] = 12
+conf["batch_size"] = 10
 conf["val_batch_size"] = 1
 conf["total_iterations"] = 25000
 conf["evaluate_rate"] =  500
@@ -17,11 +17,6 @@ conf["use_gt_bb"] = True
 conf["use_gt_pose"] = False
 conf["use_timedistributed"] = True
 conf["fine_tune"] = False 
-
-conf["learning_rate"] = 1e-2
-conf["name"] = "1e-2"
-har = HAR_PennAction(conf)
-har.run_experiment()
 
 conf["learning_rate"] = 1e-3
 conf["name"] = "1e-3"
