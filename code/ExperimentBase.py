2966f0725fd40d58e44697bea24c63d7f1139151
Matthias Jakobs <m.jakobs265@gmail.com>
2019-12-01 12:32:20 +0100

    added per joint accuracy to Pose Mixed

--- code/ExperimentBase.py
+++ code/ExperimentBase.py
@@ -13,7 +13,7 @@ import torch.optim as optim
 import torch.nn as nn
 from torch.utils.data.sampler import SubsetRandomSampler
 
-from sklearn.metrics import confusion_matrix
+#from sklearn.metrics import confusion_matrix
 
 import csv
 import numpy as np
@@ -1460,6 +1460,9 @@ class Pose_Mixed(ExperimentBase):
             
             pck_bb_02 = []
             pck_bb_01 = []
+            per_joint_accuracy = np.zeros(16)
+            number_valids = np.ones(16)
+
 
             for i in range(len(test_ds)):
                 print(i)
@@ -1491,6 +1494,12 @@ class Pose_Mixed(ExperimentBase):
 
                     pck_bb_02.extend(eval_pck_batch(predictions[:, :, 0:2], test_poses[:, :, 0:2], trans_matrices, distance_meassures, threshold=0.2))
                     pck_bb_01.extend(eval_pck_batch(predictions[:, :, 0:2], test_poses[:, :, 0:2], trans_matrices, distance_meassures, threshold=0.1))
+                    matches, valids = eval_pck_batch(predictions[:, :, 0:2], test_poses[:, :, 0:2], trans_matrices, distance_meassures, threshold=0.1, return_perjoint=True)
+
+                    number_valids = number_valids + np.array(valids[0])
+                    for u in range(16):
+                        if valids[i][u]:
+                            per_joint_accuracy[u] = per_joint_accuracy[u] + matches[0][u]
 
                     if stop == nr_frames:
                         need_stop = True
@@ -1504,6 +1513,25 @@ class Pose_Mixed(ExperimentBase):
                     del trans_matrices
                     if torch.cuda.is_available():
                         torch.cuda.empty_cache()
+
+        per_joint_final = per_joint_accuracy / number_valids
+        print(per_joint_final)
+        left_arm = per_joint_final[13] + per_joint_final[14] + per_joint_final[15]
+        right_arm = per_joint_final[10] + per_joint_final[11] + per_joint_final[12]
+        right_leg = per_joint_final[0] + per_joint_final[1] + per_joint_final[2]
+        left_leg = per_joint_final[3] + per_joint_final[4] + per_joint_final[5]
+        legs_both = left_leg + right_leg
+        arms_both = left_arm + right_arm
+        upper_body_total = arms_both + per_joint_final[8] + per_joint_final[9]
+        lower_body_total = legs_both + per_joint_final[6]
+        print("left_arm", str(left_arm / 3))
+        print("right_arm", str(right_arm / 3))
+        print("left_leg", str(left_leg / 3))
+        print("right_leg", str(right_leg / 3))
+        print("legs_both", str(legs_both / 6))
+        print("arms_both", str(arms_both / 6))
+        print("upper_body", str(upper_body_total / 8))
+        print("lower_body", str(lower_body_total / 7))
 
         print("len", str(len(pck_bb_02)))
         mean_bb_02 = torch.mean(torch.FloatTensor(pck_bb_02)).item()
